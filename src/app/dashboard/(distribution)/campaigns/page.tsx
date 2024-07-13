febf6dbdf88addf23c1df972032e88bda23bf04e
AjelmarMedina <ajelmarmedina@gmail.com>
2024-07-14 02:58:44 +0800

    Reviews

---
+++ src/app/dashboard/(distribution)/campaigns/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { PlusIcon, ShoppingBagIcon } from "lucide-react";
+
+export default function Reviews() {
+  return (
+    <main className="bg-white rounded-xl shadow-lg px-6 py-10 flex flex-col justify-center items-center text-center space-y-4">
+      <ShoppingBagIcon className="text-primary w-8 h-8"/>
+      <h1 className="font-bold text-2xl">No Campaigns Created</h1>
+      <p>Put AmazingInfluencers on autopilot with outbound email and SMS review request campaigns to your customers.</p>
+      <Button className="space-x-2">
+        <PlusIcon />
+        Create Campaign
+      </Button>
+    </main>
+  )
+}
