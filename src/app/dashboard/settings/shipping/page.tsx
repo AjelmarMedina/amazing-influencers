012ce5ceb15c1233f223ef26b89be1127bf86266
AjelmarMedina <ajelmarmedina@gmail.com>
2024-07-17 00:00:44 +0800

    Shipping & Handling

--- src/app/dashboard/settings/shipping/page.tsx
+++ src/app/dashboard/settings/shipping/page.tsx
@@ -1,5 +1,23 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
   return (
-    <></>
+    <div>
+      <div>
+        <h2 className="font-bold text-lg">Shipping & Handling</h2>
+        <p>
+          Charge your customers shipping and handling fees on product giveaways.
+        </p>
+      </div>
+      <div>
+        <h2 className="font-bold text-lg">Connect Stripe</h2>
+        <p>
+          We use Stripe to directly collect shipping/handling fees on behalf of you.
+        </p>
+      </div>
+      <Button className="self-start">
+        Connect Stripe Account
+      </Button>
+    </div>
   )
 }
