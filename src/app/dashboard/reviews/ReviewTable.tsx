e3159fd4480c3d767ac8117777df67af0a140b56
Ajelmar Medina <ajelmarmedina@gmail.com>
2024-09-08 08:15:01 +0000

    Reviews Table tweak

--- src/app/dashboard/reviews/ReviewTable.tsx
+++ src/app/dashboard/reviews/ReviewTable.tsx
@@ -17,6 +17,31 @@ export default function ReviewTable() {
   const { user } = useUser();
   const { data: reviews } = useSwr<ReviewsSchema[], any, any>(user?.primaryEmailAddress?.emailAddress, getAllReviews)
   
+  function downloadReviews() {
+    if (!reviews) return;
+    const csvStr = unparse(reviews.map((review) => {
+      return {
+        "Name": review.reviewerInfo.name,
+        "Rating": review.rating,
+        "Review": review.review,
+        "Date": review.date,
+        "Email": review.shippingInfo.email,
+        "Phone": review.shippingInfo.contactNum + " ",
+        "Address": `${review.shippingInfo.address1} ${review.shippingInfo.address2 ?? ""}`,
+        "City": review.shippingInfo.city,
+        "Full Name": review.shippingInfo.fullName,
+        "State/Province": review.shippingInfo.stateProvince,
+        "ZIP Code": review.shippingInfo.zipCode,
+      }
+    }));
+    const blob = new Blob([csvStr], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'Reviews.csv';
+    a.click();
+  }
+
   if (!reviews) return (
     <div className="w-full flex flex-col justify-center items-center">
       <section className="w-full bg-white rounded-xl shadow-lg px-6 py-10 flex flex-col justify-center items-center text-center space-y-4">
@@ -39,7 +64,7 @@ export default function ReviewTable() {
         <TableHeader className="bg-[#F3F4F6]">
             <TableRow className="text-[#343A40] font-bold *:min-w-max *:text-nowrap">
               <TableCell>Rating</TableCell>
-              <TableCell className="max-w-xs">Review</TableCell>
+              <TableCell className="min-w-64">Review</TableCell>
               <TableCell>Date</TableCell>
               <TableCell>Survey Code</TableCell>
               <TableCell>Chosen Giveaway</TableCell>
@@ -52,7 +77,7 @@ export default function ReviewTable() {
           {reviews?.map((review: ReviewsSchema) => (
             <TableRow key={review.id} className="font-medium">
               <TableCell><Rating stars={review.rating} /></TableCell>
-              <TableCell className="max-w-xs hyphens-auto">{review.review}</TableCell>
+              <TableCell className="min-w-64 hyphens-auto">{review.review}</TableCell>
               <TableCell>{review.date?.toString()}</TableCell>
               <TableCell>{review.survey?.surveyCode}</TableCell>
               <TableCell>{review.giveaway?.name}</TableCell>
@@ -77,29 +102,7 @@ export default function ReviewTable() {
       </Table>
     </div>
     <Button
-      onClick={() => {
-        const csvStr = unparse(reviews.map((review) => {
-          return {
-            "Name": review.reviewerInfo.name,
-            "Rating": review.rating,
-            "Review": review.review,
-            "Date": review.date,
-            "Email": review.shippingInfo.email,
-            "Phone": review.shippingInfo.contactNum + " ",
-            "Address": `${review.shippingInfo.address1} ${review.shippingInfo.address2 ?? ""}`,
-            "City": review.shippingInfo.city,
-            "Full Name": review.shippingInfo.fullName,
-            "State/Province": review.shippingInfo.stateProvince,
-            "ZIP Code": review.shippingInfo.zipCode,
-          }
-        }));
-        const blob = new Blob([csvStr], { type: 'text/csv' });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'Reviews.csv';
-        a.click();
-      }}
+      onClick={downloadReviews}
     >
       <DownloadIcon className="mr-2"/>
       Download Reviews
